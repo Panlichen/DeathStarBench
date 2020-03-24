8453afedc1657ee4e359d0028c5d8367f859c903
Panlichen <plch368@qq.com>
2020-03-24 16:29:15 +0800

    use nodeport for mediaservice

--- mediaMicroservices/scripts/register_users.sh
+++ mediaMicroservices/scripts/register_users.sh
@@ -2,5 +2,5 @@
 
 for i in {1..1000}; do
   curl -d "first_name=first_name_"$i"&last_name=last_name_"$i"&username=username_"$i"&password=password_"$i \
-      http://10.68.122.88:8080/wrk2-api/user/register
+      http://10.0.0.252:32000/wrk2-api/user/register
 done
